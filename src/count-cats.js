831e9524885092c13a230c9219fdff3d60eacb86
Horizon903 <taniko2425@gmail.com>
2021-03-17 22:24:11 +0200

    feat: implement count cats, carbon dating, dream-team, what-season tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+    let count = 0;
+    matrix.flat().forEach((el) => {
+        el === '^^' ? count++ : count;
+    })
+    return count;
 };
+
+
+//For example:
+//
+// countCats([ [0, 1, '^^'], [0, '^^', 2], ['^^', 1, 2] ]) => 3
