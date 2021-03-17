01e63d62225a48339e4e7666a5e56e90b009390a
Horizon903 <taniko2425@gmail.com>
2021-03-17 23:03:33 +0200

    feat: implement transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+
+  if (!Array.isArray(arr)) throw new Error();
+  if (arr.length === 0) {
+    return [];
+  }
+
+  const newArr = arr.reduce((result, num, i) => {
+    if (arr[i - 1] === '--discard-next') return result;
+    if (arr[i + 1] === '--double-prev') result.push(arr[i]);
+    if (arr[i - 1] === '--double-next') result.push(arr[i]);
+    if (arr[i + 1] === '--discard-prev') return result;
+    if (num === '--discard-next' || num === '--double-prev' || num === '--double-next' || num === '--discard-prev') {
+      return result;
+    }
+    result.push(arr[i])
+    return result;
+  }, [])
+  return newArr;
+}
